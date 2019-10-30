5fe30c5e50330d17e88c96ded49a0598b7bdf7f6
daisyarusey <daisyarusey.dc@gmail.com>
2019-10-30 17:33:07 +0300

    Initial commit

---
+++ app/src/main/java/com/moringaschool/betterreading/ui/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.betterreading.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.betterreading.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
